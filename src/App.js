d065d6422035b54bf08ed708e27f47ded249a32d
Brian Allen <brianallen2401@gmail.com>
2020-07-24 13:13:39 -0600

    adjusted baseline file structure. Started with basic navbar setup. Added bootstrap to html.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/NavbarBrand'
+import './App.css';
+
+function App() {
+  return (
+   <Navbar></Navbar>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,14 @@
+const { Navbar } = require("react-bootstrap");
+
+import React from "react";
+import "./index.css";
+
+function Navbar() {
+  return (
+  <Navbar>
+
+  </Navbar>
+  );
+}
+
+export default Navbar;
