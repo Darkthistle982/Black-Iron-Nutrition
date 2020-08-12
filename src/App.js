5c67d79fdc45be666b52bfea9828dadc33d28a62
Brian Allen <brianallen2401@gmail.com>
2020-08-12 15:31:50 -0600

    added the footer component to the app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import Nav from './Components/Nav/index'
-import './App.css';
+import React from "react";
+import Nav from "./Components/Nav/index";
+import "./App.css";
+import Footer from "./Components/Footer/index";
 
 function App() {
   return (
-   <Nav />
+    <div>
+      <Nav />
+      <Footer />
+    </div>
   );
 }
 
