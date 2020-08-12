06d5dfaa2a46193f90a33b8de1fd9b72b2c32763
Brian Allen <brianallen2401@gmail.com>
2020-08-12 15:31:14 -0600

    added footer folder and component files. Styled basic footer with message.

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./index.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Jumbotron>
+        <p className="pull-right">
+          <i className="fab fa-github" /> All items and documents within are the
+          sole property of Ink &amp; Iron Nutrition. For use by registered and
+          paid clientele of Cory Black, and Ink &amp; Iron Nutrition. Sharing of
+          login, or any of the content of this site to unauthorized parties is
+          strictly forbidden, and may result in a loss of access to the site.
+        </p>
+      </Jumbotron>
+    </footer>
+  );
+}
+
+export default Footer;
