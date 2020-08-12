152a60f1ec79e479e8b82f051ba1269b4656ed9c
Brian Allen <brianallen2401@gmail.com>
2020-08-12 15:54:04 -0600

    added the backToTop componenet to the main page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Nav from "./Components/Nav/index";
 import "./App.css";
 import Footer from "./Components/Footer/index";
+import BackToTop from "./Components/BackToTop/index";
 
 function App() {
   return (
     <div>
       <Nav />
+      <BackToTop />
       <Footer />
     </div>
   );
