425c66a37e7d801c5407d975418d106ecf8ffd9b
Brian Allen <brianallen2401@gmail.com>
2020-08-12 15:17:45 -0600

    added basic server logic to server file

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const apiRouter = require("./routes/api-routes");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} else {
+}
+
+app.use("/api", apiRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!\n`);
+});
