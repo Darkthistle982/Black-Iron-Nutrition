4213c44d57afd0f86e8cebcb0b5ae1af419225b2
Brian Allen <brianallen2401@gmail.com>
2020-08-13 12:03:08 -0600

    added collapse to navbar for responsive use.

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -5,13 +5,22 @@ import "./index.css";
 export default function Navigation() {
   return (
     <div>
-      <Navbar sticky="top" bg="dark" variant="dark">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        bg="dark"
+        variant="dark"
+      >
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#recipes">Recipes</Nav.Link>
-          <Nav.Link href="#faqs">FAQ's</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#recipes">Recipes</Nav.Link>
+            <Nav.Link href="#faqs">FAQ's</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
