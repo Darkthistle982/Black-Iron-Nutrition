bb2f9cc909a4189672a1e0ddf0e836f76859dfae
Brian Allen <brianallen2401@gmail.com>
2020-08-14 15:11:18 -0600

    added basic Hompage logic. Import and Export. No content yet. commented out switch in router. NOt needed yet.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import Nav from "./Components/Nav/index";
 import "./App.css";
 import Footer from "./Components/Footer/index";
 import BackToTop from "./Components/BackToTop/index";
+import HomePage from "./Components/HomePage/index";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Nav />
-      <BackToTop />
-      <Footer />
+      {/* <Router> */}
+        <BackToTop />
+        {/* <Switch> */}
+          {/* <Route path="/home"> */}
+            <Nav />
+            <HomePage />
+            <Footer />
+          {/* </Route>
+        </Switch>
+      </Router> */}
     </div>
   );
 }
---
+++ src/Components/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+const HomePage = () => {
+
+
+    return (
+      <div>
+          
+      </div>  
+    )
+};
+
+export default HomePage;
