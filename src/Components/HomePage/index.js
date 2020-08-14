39967e85de607893c9e31bffad6a68c279564ea7
Brian Allen <brianallen2401@gmail.com>
2020-08-14 15:59:29 -0600

    added a container and carousel with placeholders.

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -1,14 +1,48 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Carousel from "react-bootstrap/Carousel";
 import "./index.css";
 
 const HomePage = () => {
-
-
-    return (
-      <div>
-          
-      </div>  
-    )
+  return (
+    <Container fluid className="mt-2">
+      <Carousel hover="true">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/800x400/000000"
+            alt="First Slide"
+          />
+          <Carousel.Caption>
+            <h3>First Slide Label</h3>
+            <p>This is the first slide.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/800x400/000000"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h2>Second Slide Label</h2>
+            <p>This is the second slide.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/800x400/000000"
+            alt="Third Slide"
+          />
+          <Carousel.Caption>
+            <h4>Third Slide Label</h4>
+            <p>This is the third slide.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+  );
 };
 
 export default HomePage;
