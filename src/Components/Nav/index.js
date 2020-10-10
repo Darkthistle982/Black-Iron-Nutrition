292d5e66d9ebdd22107023ca639724fff21023a2
Brian Allen <brianallen2401@gmail.com>
2020-10-10 16:03:38 -0600

    added files to recipe folder. Added some router logic to navbar. needs adjusting to land on recipe home page

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,27 +1,30 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./index.css";
 
 export default function Navigation() {
   return (
-    <div>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        sticky="top"
-        bg="dark"
-        variant="dark"
-      >
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#recipes">Recipes</Nav.Link>
-            <Nav.Link href="#faqs">FAQ's</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    // <Router>
+      <div>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          sticky="top"
+          bg="dark"
+          variant="dark"
+        >
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#recipes">Recipes</Nav.Link>
+              <Nav.Link href="#faqs">FAQ's</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    // </Router>
   );
 }
---
+++ src/Components/Recipes/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+export default function Recipes() {
+  return (
+    <div>
+      <ul>
+        <li>recpies go here</li>
+      </ul>
+    </div>
+  );
+}
